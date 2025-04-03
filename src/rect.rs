ccce77b50981adfd44c5b6bade49a38ee4e4da08
Robin Oude Vrielink <robin@oudevrielink.net>
2025-04-03 17:52:30 +0200

    fix: set correct axis for rect

--- src/rect.rs
+++ src/rect.rs
@@ -9,8 +9,8 @@ impl Rect {
     pub fn new(x: i32, y: i32, w: i32, h: i32) -> Rect {
         Rect {
             x1: x,
-            x2: y,
-            y1: x + w,
+            y1: y,
+            x2: x + w,
             y2: y + h,
         }
     }
